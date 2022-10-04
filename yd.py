4c3981eed559fb2bed4271e71b5b7acacbbf5f40
Mustafa <115028591+mustafakeless@users.noreply.github.com>
2022-10-04 20:40:53 +0300

    Update yd.py

--- yd.py
+++ yd.py
@@ -1,10 +1,10 @@
 from email import header
 import requests
 from bs4 import BeautifulSoup
-header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-}
+"""header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+}"""
 
-r=requests.get("https://www.trendyol.com/laptop-x-c103108",headers=header)
+r=requests.get("https://www.trendyol.com/laptop-x-c103108")
 #print(r.status_code)
 s=BeautifulSoup(r.content,"lxml")
 laptops=s.find_all("div",attrs={"class":"p-card-wrppr with-campaign-view"})
