d911ba456b073c63c601d7a53cd3fe2488e1dfbd
Mustafa <115028591+mustafakeless@users.noreply.github.com>
2022-10-11 22:14:50 +0300

    Add files via upload

--- up.py
+++ up.py
@@ -5,50 +5,114 @@ import sqlite3
 
 con = sqlite3.connect("laptops.db")
 cursor = con.cursor()
+p=1
+while p<=3:
+    url=("https://www.trendyol.com/laptop-x-c103108?pi="+str(p)+"")
+    print(url)
+    r=requests.get(url)
+    #print(r.status_code)
+    s=BeautifulSoup(r.content,"lxml")
+    laptops=s.find_all("div",attrs={"class":"p-card-wrppr with-campaign-view"})
+    linklist=[]
+    for laptop in laptops:
+        link=laptop.find_all("div",attrs={"class":"p-card-chldrn-cntnr card-border"})
+        for f in link:
+                l=list(map(lambda lap:"https://www.trendyol.com" + lap.find("a").get("href"),link))
+                for u in l:
+                    new_string = u.replace("['","").replace("']","")
+                    linklist.append(new_string)  
+    #print(linklist)
+    for l in linklist:
+        r1=requests.get(l)
+        s1=BeautifulSoup(r1.content,"lxml")
+        ayr=s1.find_all("ul",attrs={"class":"detail-attr-container"})
+        fiyat=s1.find("span",attrs={"class":"prc-dsc"}).text.replace("TL","")
+        marka=s1.find("h1",attrs={"class":"pr-new-br"}).text.split(" ")
+        print(marka[0])
 
-r=requests.get("https://www.trendyol.com/monster/abra-a5-v16-7-3-intel-core-i5-11400h-16gb-ram-500gb-ssd-4gb-gtx1650-freedos-15-6-fhd-144hz-p-133966690")
-r=requests.get("https://www.trendyol.com/acer/an515-57-i5-11400h-8gb-ram-512ssd-4gb-3050-15-6-fhd144hz-windows-11-siyah-laptop-nh-qeley-002-p-356799756")
-r=requests.get("https://www.trendyol.com/asus/rog-strix-g15-4-nesil-ryzen-7-4800h-16gb-512gb-ssd-15-6inc-rtx3050-4gb-w11-p-349603138?boutiqueId=613344&merchantId=624588")
-r=requests.get("https://www.trendyol.com/lenovo/ideapad-gaming-3-82k100cltx-i7-11370h-16gb-1tb-256ssd-rtx3050-15-6-full-hd-freedos-tasinabili-p-294614443?boutiqueId=613344&merchantId=105013")
-r=requests.get("https://www.trendyol.com/msi/katana-gf66-11-nesil-core-i7-11800h-16gb-1tb-ssd-15-6inc-rtx3060-6gb-freedos-p-349585157?boutiqueId=613344&merchantId=624588")
-s=BeautifulSoup(r.content,"lxml")
+        """puan=s1.find("span",attrs={"class":"tltp-avg-cnt"}).text
+        print(puan)"""
+        print(fiyat)
+        for ayr2 in ayr:
+            detay=ayr2.find_all("li")
+            list=[]
+            list1=[]
+        for i in detay:
+            list.append(i.find("span").text)
+            list1.append(i.find("b").text)
+        def tabloolustur():
+            cursor.execute("CREATE TABLE IF NOT EXISTS laptop (Marka TEXT, ModelAdı TEXT, ModelNo TEXT, İşletimsistemi TEXT,İşlemcitipi TEXT,İşlemcinesli TEXT,RAM INT,DiskBoyutu INT,DiskTürü TEXT,EkranBoyutu FLOAT,Puanı FLOAT,Fiyat INT,Site TEXT)")
+        tabloolustur() 
 
-ayr=s.find_all("ul",attrs={"class":"detail-attr-container"})
-fiyat=s.find("span",attrs={"class":"prc-dsc"}).text.replace("TL","").replace(",99","")
+            
+        for j1 in list:
+            a=0
+            if j1=="İşlemci Tipi":
+                islemcitipi=list1[a]
+            a=a+1                 
+        a=0
+        for j3 in list:
+                if j3=="İşletim Sistemi":
+                    isletimsistemi=list1[a]
+                a=a+1    
+        a=0            
+        for j4 in list:
+                if j4=="Ram (Sistem Belleği)":
+                    ram=list1[a]
+                a=a+1                     
+        a=0
+        for j5 in list:
+                if j5=="Ekran Boyutu":
+                    ekranboyutu=list1[a]
+                a=a+1                
+        a=0
+        for j6 in list:
+                if j6=="İşlemci Nesli":
+                    islemcinesli=list1[a]
+                a=a+1                 
+        a=0
+        for j7 in list:
+                if j7=="İşlemci Nesli":
+                    islemcinesli=list1[a]
+                a=a+1                 
+        a=0
+        for j8 in list:
+                if j8=="SSD Kapasitesi":
+                    ssdiskboyutu=list1[a]
+                    if ssdiskboyutu=="SSD Yok":
+                        ssdiskboyutu=""     
+                a=a+1                 
+        a=0
+        for j8 in list:
+                if j8=="Hard Disk Kapasitesi":
+                    hddiskboyutu=list1[a]
+                    if hddiskboyutu=="HDD Yok":
+                        hddiskboyutu=""         
+                a=a+1                 
+        a=0
+        for j2 in list1:
+                if j2=="SSD Yok":
+                    diskturu="HDD"
+                    diskboyutu=hddiskboyutu
+                    break 
+                elif j2=="HDD Yok":
+                    diskturu="SSD"
+                    diskboyutu=ssdiskboyutu
+                    break
+                else:
+                    diskturu="HDD+SSD"
+                    diskboyutu=hddiskboyutu+"+"+ssdiskboyutu        
+                a=a+1           
+        a=0
+        print(islemcitipi,isletimsistemi,diskturu,ram,diskboyutu,ekranboyutu,islemcinesli)
+        def degerekle():
+                cursor.execute("INSERT INTO laptop VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)", (marka[0],marka[1]+" "+marka[2],marka[3],isletimsistemi,islemcitipi,islemcinesli,ram,diskboyutu,diskturu,ekranboyutu,9.0,fiyat,'Trendyol'))
+                con.commit()
+                #con.close()      
+        degerekle()
+    p=p+1
+        #print(l)
 
-marka=s.find("h1",attrs={"class":"pr-new-br"}).text.split(" ")
-print(marka[0])
-
-print(fiyat)
-for ayr2 in ayr:
-    detay=ayr2.find_all("li")
-    list=[]
-    list1=[]
-    
-for i in detay:
-    list.append(i.find("span").text)
-    list1.append(i.find("b").text)
-
-def tabloolustur():
-    cursor.execute("CREATE TABLE IF NOT EXISTS laptop (Marka TEXT, ModelAdı TEXT, ModelNo TEXT, İşletimsistemi TEXT,İşlemcitipi TEXT,İşlemcinesli TEXT,RAM INT,DiskBoyutu INT,DiskTürü TEXT,EkranBoyutu FLOAT,Puanı FLOAT,Fiyat INT,Site TEXT)")
-tabloolustur() 
-def degerekle1():
-        cursor.execute("INSERT INTO laptop VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)", (marka[0],marka[1]+" "+marka[2],marka[3],list1[2],list1[0],list1[30],list1[5],list1[1],"SSD",list1[15],9.0,fiyat,'Trendyol'))
-        con.commit()
-def degerekle2():
-        cursor.execute("INSERT INTO laptop VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)", (marka[0],marka[1]+" "+marka[2],marka[3],list1[1],list1[0],list1[14],list1[3],list1[4],"HDD",list1[19],9.0,fiyat,'Trendyol'))
-        con.commit()
-        
-for j in list1:
-    if j=="HDD Yok":
-        degerekle1()
-        con.close()  
-    elif j=="SSD Yok":
-        degerekle2()
-        con.close()          
-      
-print(list)
-print(list1)
 
 
                  
--- yd11.py
+++ yd11.py
@@ -1,16 +1,108 @@
 from email import header
+from pyexpat import model
+from time import sleep
 import requests
 from bs4 import BeautifulSoup
-"""header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-}"""
+import sqlite3
 
-r=requests.get("https://www.n11.com/bilgisayar/dizustu-bilgisayar")
-#print(r.status_code)
-s=BeautifulSoup(r.content,"lxml")
-laptops=s.find_all("li",attrs={"class":"column"})
-#print(laptops)
-for laptop in laptops:
-    link=laptop.find_all("div",attrs={"class":"pro"})
-    for f in link:
-        a=list(map(lambda lap: lap.find("a").get("href"),link)) 
-        print(a[0])
+con = sqlite3.connect("laptops.db")
+cursor = con.cursor()
+p=1
+for page in range(1,4):
+    url="https://www.n11.com/bilgisayar/dizustu-bilgisayar?ipg={}".format(page)
+    print(url)
+    r=requests.get(url).text
+    s=BeautifulSoup(r,"lxml")
+    laptops=s.find_all("li",attrs={"class":"column"})
+    linklist=[]
+    for laptop in laptops:
+        link=laptop.find_all("div",attrs={"class":"pro"})
+        for f in link:
+                a=list(map(lambda lap: lap.find("a").get("href"),link))
+                for u in a:
+                    new_string = u.replace("['","").replace("']","")
+                    linklist.append(new_string)  
+    #print(linklist)
+    for a in linklist:
+        r1=requests.get(a)
+        s1=BeautifulSoup(r1.content,"lxml")
+        ayr=s1.find_all("div",attrs={"class":"unf-prop-context"})
+        #fiyat=s1.find("div",attrs={"class":"newPrice"}).text.replace("TL","").replace(",99","")
+        #marka=s1.find("h1",attrs={"class":"proName"}).text.split(" ")
+        #print(marka[0])
+        
+        """try:
+            puan=s1.find("strong").text
+        except:
+            puan="NR"
+        print(float(puan))"""
+        
+        for ayr2 in ayr:
+            detay=ayr2.find_all("li",attrs={"class":"unf-prop-list-item"})
+            list=[]
+            list1=[]
+        for i in detay:
+            list.append(i.find("p",attrs={"class":"unf-prop-list-title"}).text)
+            list1.append(i.find("p",attrs={"class":"unf-prop-list-prop"}).text)
+        def tabloolustur():
+            cursor.execute("CREATE TABLE IF NOT EXISTS laptop (Marka TEXT, ModelAdı TEXT, ModelNo TEXT, İşletimsistemi TEXT,İşlemcitipi TEXT,İşlemcinesli TEXT,RAM INT,DiskBoyutu INT,DiskTürü TEXT,EkranBoyutu FLOAT,Puanı FLOAT,Fiyat INT,Site TEXT)")
+        tabloolustur() 
+
+        a=0    
+        for j1 in list:
+        
+            if j1=="İşlemci":
+                islemcitipi=list1[a]
+            a=a+1                 
+        a=0
+        for j3 in list:
+                if j3=="İşletim Sistemi":
+                    isletimsistemi=list1[a]
+                a=a+1    
+        a=0            
+        for j4 in list:
+                if j4=="Bellek Kapasitesi":
+                    ram=list1[a]
+                a=a+1                     
+        a=0
+        for j5 in list:
+                if j5=="Ekran Boyutu":
+                    ekranboyutu=list1[a]
+                a=a+1                
+        a=0
+        for j6 in list:
+                if j6=="İşlemci Modeli":
+                    islemcinesli=list1[a]
+                a=a+1                 
+        a=0
+        for j7 in list:
+                if j7=="Disk Türü":
+                    diskturu=list1[a]
+                a=a+1                 
+        a=0
+        for j8 in list:
+                if j8=="Disk Kapasitesi":
+                    diskboyutu=list1[a]     
+                a=a+1                 
+        a=0
+        for j9 in list:
+                if j9=="Marka":
+                    marka=list1[a]     
+                a=a+1                 
+        a=0
+        for j2 in list:
+                if j2=="Model":
+                    model=list1[a]     
+                a=a+1                 
+        a=0
+        print(islemcitipi,isletimsistemi,diskturu,ram,diskboyutu,ekranboyutu,islemcinesli)
+        def degerekle():
+                cursor.execute("INSERT INTO laptop VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)", (marka,marka,model,isletimsistemi,islemcitipi,islemcinesli,ram,diskboyutu,diskturu,ekranboyutu,9.0,0.0,'n11'))
+                con.commit()
+                #con.close()      
+        degerekle()
+    p=p+1
+
+
+                 
+       
